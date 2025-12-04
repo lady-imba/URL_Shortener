3cfc84807d1e43b906876ad4cb24fb83985ed770
user <user@MacBook-Pro-user.local>
2025-12-04 12:41:15 +0300

    Создала логгер

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"URL_SHORTENER/internal/config"
-	"fmt"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
 )
 
 func main(){
 	config := config.MustLoad()
 
-	fmt.Print(config)
+	log := setupLogger(config.Env)
+
+	log.Info("Starting url-shrtener", slog.String("env", config.Env))
 
 	//TODO: init logger
 
@@ -18,3 +27,26 @@ func main(){
 
 	//TODO: run server
 }
+
+func setupLogger(env string)*slog.Logger{
+	var log *slog.Logger
+
+	switch env{
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout,&slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout,&slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout,&slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log 
+}
+//локально хотим видеть текстовые логги
+//в проде или дев хотим видеть json
